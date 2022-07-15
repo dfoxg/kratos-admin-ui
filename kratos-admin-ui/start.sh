a135a937ce8e228c65a0cc6a359471e9e41d357a
Daniel Fuchs <daniel.fuchs2211@gmail.com>
2022-07-15 17:23:29 +0200

    activate renovate; change env checker location

--- kratos-admin-ui/start.sh
+++ kratos-admin-ui/start.sh
@@ -1,2 +1,11 @@
+if [ -z ${KRATOS_ADMIN_URL+x} ]; then 
+    echo "KRATOS_ADMIN_URL is unset. Please set the environment variable 'KRATOS_ADMIN_URL'";
+    exit 2
+fi
+if [ -z ${KRATOS_PUBLIC_URL+x} ]; then 
+    echo "KRATOS_PUBLIC_URL is unset.  Please set the environment variable 'KRATOS_PUBLIC_URL'";
+    exit 3
+fi
+
 sh env2conf.sh /usr/share/nginx/html/config.json
 nginx -g "daemon off;"
