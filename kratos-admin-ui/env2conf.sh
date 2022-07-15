9bfb5d963b23e6f7ce86287ccabd43b6289039a0
Daniel Fuchs <daniel.fuchs2211@gmail.com>
2022-07-15 17:16:39 +0200

    show error message if environment variable is not set

--- kratos-admin-ui/env2conf.sh
+++ kratos-admin-ui/env2conf.sh
@@ -3,6 +3,16 @@
 # Usage: env2conf dir filename
 # env2conf.sh public/config.json
 ##
+
+if [ -z ${KRATOS_ADMIN_URL+x} ]; then 
+    echo "KRATOS_ADMIN_URL is unset. Please set the environment variable 'KRATOS_ADMIN_URL'";
+    exit 2
+fi
+if [ -z ${KRATOS_PUBLIC_URL+x} ]; then 
+    echo "KRATOS_PUBLIC_URL is unset.  Please set the environment variable 'KRATOS_PUBLIC_URL'";
+    exit 3
+fi
+
 mkdir -p "$(dirname "$1")" && touch "$1"
 echo '{' >"$1"
 echo '   "kratosAdminURL":"'$KRATOS_ADMIN_URL'",' >> "$1"
