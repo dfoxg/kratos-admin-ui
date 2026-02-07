296cfa808bd7b2aae2cee82484140b83cf53b484
Daniel Fuchs <d@dfoxg.org>
2026-02-07 10:23:00 +0100

    refactor cors proxy

--- kratos-admin-ui/cors-proxy.js
+++ kratos-admin-ui/cors-proxy.js
@@ -4,61 +4,57 @@ http.createServer(onRequestAdmin).listen(4435);
 http.createServer(onRequestPublic).listen(4430);
 
 console.log("listening ...");
+function getCorsHeaders(client_req) {
+  return {
+    "access-control-allow-origin": "http://localhost:5173",
+    "access-control-allow-credentials": "true",
+    "access-control-allow-headers":
+      client_req.headers["access-control-request-headers"] || "Content-Type",
+    "access-control-allow-methods":
+      "GET, PUT, DELETE, PATCH, HEAD, OPTIONS, POST",
+  };
+}
 
-function onRequestAdmin(client_req, client_res) {
-  console.log("serve: " + client_req.url);
+function handlePreflight(client_req, client_res) {
+  if (client_req.method === "OPTIONS") {
+    client_res.writeHead(200, getCorsHeaders(client_req));
+    client_res.end();
+    return true;
+  }
+  return false;
+}
 
+function proxyToPort(port, client_req, client_res) {
   var options = {
     hostname: "127.0.0.1",
-    port: 4434,
+    port: port,
     path: client_req.url,
     method: client_req.method,
     headers: client_req.headers,
   };
 
   var proxy = http.request(options, function (res) {
-    const headers = res.headers;
-    headers["access-control-allow-origin"] = "http://localhost:5173";
-    headers["access-control-allow-credentials"] = "true";
-    headers["access-control-allow-headers"] = "Content-Type";
-    headers["access-control-allow-methods"] =
-      "GET, PUT, DELETE, PATCH, HEAD, OPTIONS, POST";
+    const headers = res.headers || {};
+    Object.assign(headers, getCorsHeaders(client_req));
     client_res.writeHead(res.statusCode, headers);
-    res.pipe(client_res, {
-      end: true,
-    });
+    res.pipe(client_res, { end: true });
   });
 
-  client_req.pipe(proxy, {
-    end: true,
-  });
+  client_req.pipe(proxy, { end: true });
+}
+
+function onRequestAdmin(client_req, client_res) {
+  console.log("serve: " + client_req.url);
+
+  if (handlePreflight(client_req, client_res)) return;
+
+  proxyToPort(4434, client_req, client_res);
 }
 
 function onRequestPublic(client_req, client_res) {
   console.log("serve: " + client_req.url);
 
-  var options = {
-    hostname: "127.0.0.1",
-    port: 4433,
-    path: client_req.url,
-    method: client_req.method,
-    headers: client_req.headers,
-  };
+  if (handlePreflight(client_req, client_res)) return;
 
-  var proxy = http.request(options, function (res) {
-    const headers = res.headers;
-    headers["access-control-allow-origin"] = "http://localhost:5173";
-    headers["access-control-allow-credentials"] = "true";
-    headers["access-control-allow-headers"] = "Content-Type";
-    headers["access-control-allow-methods"] =
-      "GET, PUT, DELETE, PATCH, HEAD, OPTIONS, POST";
-    client_res.writeHead(res.statusCode, headers);
-    res.pipe(client_res, {
-      end: true,
-    });
-  });
-
-  client_req.pipe(proxy, {
-    end: true,
-  });
+  proxyToPort(4433, client_req, client_res);
 }
